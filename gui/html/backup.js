db07d85afca1257c89cc570b3745e7bd50771111
lbcmk <30442978+lbcmk@users.noreply.github.com>
2022-09-09 20:30:28 -0400

    Backup all button works

--- gui/html/backup.js
+++ gui/html/backup.js
@@ -1,5 +1,9 @@
 function backupAllFolders() {
-    console.log("backup")
+    folders = document.querySelectorAll("div.folder.list")
+    for(i=0; i<folders.length; i++) {
+        runBackupFolders(folders[i].id, "backup")
+    }
+
 }
 
 // Function for the backup button
@@ -61,8 +65,8 @@ function runBackupFolders(selfID, format = "folders") {
             addTextToFoldersTextBox(data["dir1"]["new_folders"], "greenText", "New Folders:", "+") // New Folders
             addTextToFoldersTextBox(data["dir1"]["new_files"], "greenText", "New Files:", "+") // New Files
             addTextToFoldersTextBox(data["edited_files"], "redText", "Changed Files:", "\u2022") // Changed Files
-            // addTextToFoldersTextBox(data["dir2"]["new_folders"], "redText", "Removed Folders:", "-") // Removed Folders
-            // addTextToFoldersTextBox(data["dir2"]["new_files"], "redText", "Removed Files:", "-") // Removed Files
+            addTextToFoldersTextBox(data["dir2"]["new_folders"], "redText", "Removed Folders:", "-") // Removed Folders
+            addTextToFoldersTextBox(data["dir2"]["new_files"], "redText", "Removed Files:", "-") // Removed Files
         }
     })
       .catch((error) => {
